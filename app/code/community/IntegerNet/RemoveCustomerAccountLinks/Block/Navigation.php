6b1fd67de3c709c9aa7741627df0ea026d37e8e3
Viktor Franz <vf@integer-net.de>
2013-01-15 18:36:29 +0100

    Magento version independent and refactoring
    - Fetch all customer account navigation links from layout-block
    - Add block comments

--- app/code/community/IntegerNet/RemoveCustomerAccountLinks/Block/Navigation.php
+++ app/code/community/IntegerNet/RemoveCustomerAccountLinks/Block/Navigation.php
@@ -1,27 +1,31 @@
 <?php
-/**
- * User: Christian Philipp
- * Date: 13.09.12
- * Time: 13:12
- * To change this template use File | Settings | File Templates.
- */
 
+/**
+ *
+ * @category    IntegerNet
+ * @package     IntegerNet_RemoveCustomerAccountLinks
+ * @author      Christian Philipp <cp@integer-net.de>
+ */
 class IntegerNet_RemoveCustomerAccountLinks_Block_Navigation extends Mage_Customer_Block_Account_Navigation
 {
+
+
     public function removeLink()
     {
         /*
          * hide account links choosen in backend configuration
          */
 
-        $pages = Mage::getStoreConfig('removecustomeraccountlinks/settings/remove');
-        $pagesArray = explode(',',$pages);
+        $pages = Mage::getStoreConfig(IntegerNet_RemoveCustomerAccountLinks_Helper_Data::XML_PATH_REMOVECUSTOMERACCOUNTLINKS_SETTINGS_REMOVE);
+        $pagesArray = explode(',', $pages);
 
         foreach ($pagesArray as $name) {
             unset($this->_links[$name]);
         }
+
         return $this;
     }
+
 
     protected function _toHtml()
     {
